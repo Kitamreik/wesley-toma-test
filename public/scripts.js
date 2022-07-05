f6517ec485c0c2435f474deacb3f3b94056ea380
kitamreik <kitdamreik@gmail.com>
2022-07-05 19:22:21 -0400

    psuedocode

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,42 @@
+// DOM Event Steps:
+
+// 1. select hamburger icon --> event target
+const hamburgerIcon = document.querySelector("");
+const navUl = document.querySelector("");
+
+// 2. event handler --> show/hide
+
+// 3. bind with event listener --> attach hamburger icon to event listener, NOT Menu Items
+hamburgerIcon.addEventListener("click", () => {
+    navUl.classList.toggle("open-sesame");
+});
+
+
+
 // loading bar functionality
 
+
+
 // back to top arrow button
+
+//Get the button
+let scrollbutton = document.getElementById("scroll-button");
+
+// When the user scrolls down 2px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    // 200 works
+    // limit
+  if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
+    scrollbutton.style.display = "block";
+  } else {
+    scrollbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
